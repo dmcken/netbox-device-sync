866e4fde0c7aefeaef92ddb3c6870084e7a8fc34
David Mc Ken <david.mcken@gmail.com>
2022-02-20 11:11:17 +0000

    Added parse_device_parameters function.

--- utils.py
+++ utils.py
@@ -1,7 +1,25 @@
-
+'''
+General Utility definitions and functions
+'''
 
 
 
 import ipaddress
+import re
 
 link_local_subnet = ipaddress.ip_network('fe80::/10')
+
+def parse_device_parameters(config):
+    # Parse the config parameters
+    device_credentials = {}
+    for curr_dev_attr in dir(config):
+        attr_re = re.match("DEV_([A-Za-z0-9]+)", curr_dev_attr)
+        if not attr_re:
+            continue
+
+        attr_value = getattr(config, curr_dev_attr)
+
+        if not attr_value:
+            continue
+
+        device_credentials[attr_re.group(1).lower()] = attr_value
