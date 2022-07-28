f7f39b7da0d0ff95940a8e4467a1f560e5f839b0
David Mc Ken <david.mcken@gmail.com>
2022-07-28 15:18:48 +0000

    Scaffolding for edgeos

---
+++ drivers/edgeos.py
@@ -0,0 +1,9 @@
+'''
+Driver for Ubiquiti EdgeOS devices (EdgeRouter)
+
+
+Notes:
+Easiest method seems to be fetch config via SCP /config/config.boot
+Parse using vyatta-conf-parser (I already have a fork of it).
+
+'''
