5846f4e3f92ea430d417224276c534471f115d1c
David Mc Ken <david.mcken@gmail.com>
2024-07-24 04:18:10 +0000

    Update utils.py Add docstring to parse_device_parameters. Add import comment.

--- utils.py
+++ utils.py
@@ -3,14 +3,23 @@ General Utility definitions and functions
 '''
 
 
-
+# System imports
 import ipaddress
 import re
+
+
 
 link_local_subnet = ipaddress.ip_network('fe80::/10')
 
 def parse_device_parameters(config):
-    # Parse the config parameters
+    """Parse the config parameters.
+
+    Args:
+        config (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
     device_credentials = {}
     for curr_dev_attr in dir(config):
         attr_re = re.match("DEV_([A-Za-z0-9]+)", curr_dev_attr)
